b205e35bf5a509069e488285fdc68b1bd227d9cc
Mr. Squid <mrsquid4744@gmail.com>
2023-12-16 23:18:43 +0900

    drop region

--- db-gen.py
+++ db-gen.py
@@ -7,8 +7,8 @@ con = sqlite3.connect("ip2geo.sq3")
 cur = con.cursor()
 cur.execute("PRAGMA foreign_keys = ON;")
 cur.execute("CREATE TABLE strings (id INTEGER, string TEXT UNIQUE, PRIMARY KEY(id AUTOINCREMENT));")
-cur.execute("CREATE TABLE geolite2 (ip_start_0 INT, ip_start_1 INT, ip_start_2 INT, ip_start_3 INT, ip_end_0 INT, ip_end_1 INT, ip_end_2 INT, ip_end_3 INT, country_code INT, country_name INT, state INT, region INT, city INT, CONSTRAINT 'cns1' FOREIGN KEY('country_code') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT 'cns2' FOREIGN KEY('country_name') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT 'cns3' FOREIGN KEY('state') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT 'cns4' FOREIGN KEY('region') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT 'cns5' FOREIGN KEY('city') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE);")
-cur.execute("CREATE VIEW geolite2_view AS SELECT a.ip_start_0,a.ip_start_1,a.ip_start_2,a.ip_start_3,a.ip_end_0,a.ip_end_1,a.ip_end_2,a.ip_end_3,b.string AS country_code,c.string AS country_name,d.string AS state,e.string AS region,f.string AS city from geolite2 a join strings b on a.country_code = b.id join strings c on a.country_name = c.id join strings d on a.state = d.id join strings e on a.region = e.id join strings f on a.city = f.id;")
+cur.execute("CREATE TABLE geolite2 (ip_start_0 INT, ip_start_1 INT, ip_start_2 INT, ip_start_3 INT, ip_end_0 INT, ip_end_1 INT, ip_end_2 INT, ip_end_3 INT, country_code INT, country_name INT, state INT, city INT, CONSTRAINT 'cns1' FOREIGN KEY('country_code') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT 'cns2' FOREIGN KEY('country_name') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT 'cns3' FOREIGN KEY('state') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT 'cns5' FOREIGN KEY('city') REFERENCES strings(id) ON DELETE CASCADE ON UPDATE CASCADE);")
+cur.execute("CREATE VIEW geolite2_view AS SELECT a.ip_start_0,a.ip_start_1,a.ip_start_2,a.ip_start_3,a.ip_end_0,a.ip_end_1,a.ip_end_2,a.ip_end_3,b.string AS country_code,c.string AS country_name,d.string AS state,f.string AS city from geolite2 a join strings b on a.country_code = b.id join strings c on a.country_name = c.id join strings d on a.state = d.id join strings f on a.city = f.id;")
 #con.set_trace_callback(print)
 
 num_lines = 0
@@ -26,7 +26,7 @@ with open('geolite2-city-ipv4.csv','r') as fin:
     cur.execute("INSERT INTO strings (string) VALUES('{str}');".format(str = ''))
     con.commit()
     for i in dr:
-        country_code, country_name, state, region, city= 1, 1, 1, 1, 1
+        country_code, country_name, state, city= 1, 1, 1, 1
         cname = ''
         if pycountry.countries.get(alpha_2=i['country_code']) != None:
             cname = pycountry.countries.get(alpha_2=i['country_code']).name
@@ -56,13 +56,6 @@ with open('geolite2-city-ipv4.csv','r') as fin:
             state = cur.fetchone()[0]
             pass
         try:
-            cur.execute("INSERT INTO strings (string) VALUES (?);", (i['region'],))
-            region = cur.lastrowid
-        except sqlite3.Error as er:
-            cur.execute("SELECT id FROM strings WHERE string = ?;", (i['region'],))
-            region = cur.fetchone()[0]
-            pass
-        try:
             cur.execute("INSERT INTO strings (string) VALUES (?);", (i['city'],))
             city = cur.lastrowid
         except sqlite3.Error as er:
@@ -70,7 +63,7 @@ with open('geolite2-city-ipv4.csv','r') as fin:
             city = cur.fetchone()[0]
             pass
 
-        cur.execute("INSERT INTO geolite2 (ip_start_0, ip_start_1, ip_start_2, ip_start_3, ip_end_0, ip_end_1, ip_end_2, ip_end_3, country_code, country_name, state, region, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", (i['ip_start'].split(".")[0], i['ip_start'].split(".")[1], i['ip_start'].split(".")[2], i['ip_start'].split(".")[3], i['ip_end'].split(".")[0], i['ip_end'].split(".")[1], i['ip_end'].split(".")[2], i['ip_end'].split(".")[3], country_code, country_name, state, region, city))
+        cur.execute("INSERT INTO geolite2 (ip_start_0, ip_start_1, ip_start_2, ip_start_3, ip_end_0, ip_end_1, ip_end_2, ip_end_3, country_code, country_name, state, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", (i['ip_start'].split(".")[0], i['ip_start'].split(".")[1], i['ip_start'].split(".")[2], i['ip_start'].split(".")[3], i['ip_end'].split(".")[0], i['ip_end'].split(".")[1], i['ip_end'].split(".")[2], i['ip_end'].split(".")[3], country_code, country_name, state, city))
         ins_count = ins_count + 1
         if ins_count % 100 == 0:
             prog = (100.0 / num_lines) * ins_count
