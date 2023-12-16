8b0f641ef7228ae6603dacad91b827a44ed1a7e8
Mr. Squid <mrsquid4744@gmail.com>
2023-12-17 03:13:28 +0900

    download & pbars

--- db-gen.py
+++ db-gen.py
@@ -1,4 +1,35 @@
-import csv, sqlite3, pycountry, os, time, traceback, sys
+import csv, sqlite3, pycountry, os, time, traceback, sys, urllib.request, gzip, shutil
+from tqdm import tqdm
+
+pbar = None
+last = 0
+should_del = True
+def show_progress(block_num, block_size, total_size):
+    global pbar
+    global last
+    if pbar is None:
+        pbar = tqdm(total=total_size, desc='Downloading geolite2... ')
+
+    downloaded = block_num * block_size
+    if downloaded < total_size:
+        pbar.update(downloaded - last)
+        last = downloaded
+    else:
+        pbar.close()
+        pbar = None
+
+if os.path.exists("geolite2-city-ipv4.csv"):
+    print('Geolite2 found, skipping download')
+    should_del = False
+else:
+    urllib.request.urlretrieve("https://github.com/sapics/ip-location-db/raw/main/geolite2-city/geolite2-city-ipv4.csv.gz", "geolite2-city-ipv4.csv.gz", show_progress)
+
+    print('Extracting geolite2... ', end='')
+    with gzip.open('geolite2-city-ipv4.csv.gz', 'rb') as f_in:
+        with open('geolite2-city-ipv4.csv', 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    os.remove("geolite2-city-ipv4.csv.gz")
+    print('OK')
 
 if os.path.exists("ip2geo.sq3"):
     os.remove("ip2geo.sq3")
@@ -21,6 +52,8 @@ except Exception as e:
 print("Found " + str(num_lines) + " total records")
 
 ins_count = 0
+bbar = None
+bbar_last = 0
 with open('geolite2-city-ipv4.csv','r') as fin:
     dr = csv.DictReader(fin, fieldnames=["ip_start", "ip_end", "country_code", "state", "region", "city", "post_code", "latitude", "longitude", "timezone"])
     cur.execute("INSERT INTO strings (string) VALUES('{str}');".format(str = ''))
@@ -65,12 +98,18 @@ with open('geolite2-city-ipv4.csv','r') as fin:
 
         cur.execute("INSERT INTO geolite2 (ip_start_0, ip_start_1, ip_start_2, ip_start_3, ip_end_0, ip_end_1, ip_end_2, ip_end_3, country_code, country_name, state, city) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", (i['ip_start'].split(".")[0], i['ip_start'].split(".")[1], i['ip_start'].split(".")[2], i['ip_start'].split(".")[3], i['ip_end'].split(".")[0], i['ip_end'].split(".")[1], i['ip_end'].split(".")[2], i['ip_end'].split(".")[3], country_code, country_name, state, city))
         ins_count = ins_count + 1
+        if bbar is None:
+            bbar = tqdm(total=num_lines, desc='Building database... ')
         if ins_count % 100 == 0:
-            prog = (100.0 / num_lines) * ins_count
-            print('Building database... [%d%%]\r'%prog, end="")
-    print('Building database... [%d%%]\r'%100, end="")
+            bbar.update(ins_count - bbar_last)
+            bbar_last = ins_count
+    bbar.close()
 
 con.commit()
 con.close()
 
-print("\n\nDone")
+if should_del:
+    if os.path.exists("geolite2-city-ipv4.csv"):
+        os.remove("geolite2-city-ipv4.csv")
+
+print("\nDone")
